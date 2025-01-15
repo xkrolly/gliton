32973b4d0023e4eefdad05989732f99a3647bbc2
Xkrolly <xkrolly@gmail.com>
2025-01-15 20:52:55 +0100

    upd index

--- index.php
+++ index.php
@@ -213,11 +213,12 @@ $page="
     <meta name='application-name' content='Glit'>
     <meta name='description' content='Seek professional or technical guide/advice from expert!'>
     <meta name='Homepage' content='GLit homepage'>
-    <meta name='theme-color' content='#2166f3'>
-    <meta name='msapplication-TileColor' content='#2166f3'>
+    <meta name='theme-color' content='#fff'>
+    <meta name='msapplication-TileColor' content='#fff'>
     <meta name='apple-mobile-web-app-capable' content='yes'>
     <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'>
-    <meta name='glit:title' content='GLit homepage'>
+    <meta name='title' content='Glit'>
+    <meta name='glit:title' content='Glit'>
     <meta name='glit:description' content='Seek technical guide as urgent as possible. Get started with GLit now!'>
     <meta name='glit:creator' content='@_GLit'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover, maximum-scale=1, user-scalable=no'>
