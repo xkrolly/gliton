c615d17e5a2fd5bbabe2eb8eb41cc3c8a96e32ff
Xkrolly <xkrolly@gmail.com>
2025-01-26 02:49:52 +0100

    trouble shooting prfile pg

--- classes/usersview.class.php
+++ classes/usersview.class.php
@@ -4170,8 +4170,8 @@ $scriptPanel .="<div id='box' style='margin-bottom:1px; height:100vh; border-bot
 					        	<source src='videos/vet/aaa.mp4' id='srcmediaB'/>
         					</video>
         				</div> 
-        				<figcaption style='height:160px; text-align:left; z-index:10; padding:2px; position:absolute; top:72%; left:10px; color:#fff; width:45%;'>
-                                <div style='z-index:11; padding:15px 10px 15px 15px; width:100%; border-radius:10px; display:inline-flex; flex-direction:column; margin:2px; background-image:radial-gradient(rgba(255, 255, 255, 0.1), transparent);'>
+        				<figcaption style='height:160px; text-align:left; z-index:10; padding:2px; position:absolute; top:72%; left:1px; color:#fff; width:45%;'>
+                                <div style='z-index:11; padding:1px; width:100%; border-radius:10px; display:inline-flex; flex-direction:column; margin:1px; background-image:radial-gradient(rgba(255, 255, 255, 0.1), transparent);'>
                                 <div style=''><span style='font-size:22px; text-shadow:1px 1px 1px #444; color:#fff; font-weight:bold;' id='uname'>@username</span></div>
                                 <div style='margin-top:-6px; width:110px; height:2px; background:#fff; color:#fff; filter: drop-shadow(1px 1px 1px #444);'></div>
                                 <div style='margin-top:-10px;'><span style='font-size:13px; text-shadow:1px 1px 1px #444; color:#fff; font-weight:bold;'>
