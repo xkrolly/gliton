25120e74b7679c7b2ea8402d88909a582b4717d8
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-02-15 07:44:24 +0100

    Update usersview.class.php

--- classes/usersview.class.php
+++ classes/usersview.class.php
@@ -5651,10 +5651,10 @@ $Chats .="    					             <span style='font-size:10px; color:#000; margin-
         <div style="background:#fff; padding:10px; display:flex; align-items:center; justify-content:center;">
 
               <div style="border:2px solid #aaa; border-radius:20px; display:flex; justify-content:center; align-items:center;">
-              <button type="submit" name="submit" tabindex=3 class="" id="search_btn" style="width:9vw;  background:transparent; border:transparent; border-top-left-radius:15px; border-bottom-left-radius:15px; padding:3px; text-align:center;">
-                  <span class="material-icons" style="color:#000; font-size:30px; margin-right:2px; margin-left:5px;">&#xe8b6;</span>
+              <button type="submit" name="submit" tabindex=3 class="" id="search_btn" style="width:9vw;  background:#fff; border:transparent; border-top-left-radius:15px; border-bottom-left-radius:15px; padding:3px 8px 3px 3px; text-align:center;">
+                  <span class="material-icons" style="color:#000; font-size:25px; margin-right:2px; margin-left:5px;">&#xe8b6;</span>
               </button>
-              <input type="search" id="searchInput" incremental name="search_input" placeholder="Search any solution..." aria-label="Search the pool of published practical solutions" style="padding-left:8px color:#fff; font-size:16px; width:70vw; border:#fff; background:#fff; border-bottom-right-radius:15px; border-top-right-radius:15px;">
+              <input type="search" id="searchInput" incremental name="search_input" placeholder="Search any solution..." aria-label="Search the pool of published practical solutions" style="padding-left:8px color:#fff; font-size:16px; width:70vw; border:#fff; background:#fff;">
 							
 							<div style="text-align:center; margin-right:10px;" onclick="document.getElementById(\'searchInput\').value=\'\';"> x </div>
            </div>
