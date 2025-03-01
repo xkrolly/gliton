6151f067b155d426649b573507174c9d026657a4
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-03-01 18:05:17 +0100

    Update peepChats.php

--- views/peepChats.php
+++ views/peepChats.php
@@ -10,7 +10,8 @@ if($regCompleted == 1){
     
         if(isset($_GET['pub'])){
           //$_GET['solo'] == $usersView->encryptor0('solo');
-          $pub_id = $usersView->decryptor0( str_replace(' ', '+', $_GET['pub']) );
+        //  $pub_id = $usersView->decryptor0( str_replace(' ', '+', $_GET['pub']) );
+          $pub_id = $usersView->alphaA_Num($_GET['pub']);
     
           //get the pub_id
           $pub_data = $usersView->select('publish', ' WHERE pub_id = ?', $pub_id);
