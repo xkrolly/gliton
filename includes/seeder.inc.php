13d1856d9f0b6dd267e4a619306b67ade3c443e6
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-03-15 08:10:48 +0100

    Create seeder.inc.php

---
+++ includes/seeder.inc.php
@@ -0,0 +1,155 @@
+<?php
+include('autoloader.inc.php');
+
+	$usersContr = new usersContr();
+	$usersView = new usersView();
+
+if(isset($_POST['problem'])){
+	$topic = $_POST['topic'];
+	$insight = $_POST['insight'];
+
+	$cat = $_POST['category'];
+	$datetime = $_POST['date'].' '.$_POST['time'];
+
+	    $aoi_array = $usersView->aoi($cat);
+		$folder = $aoi_array['folder'];
+
+	$que = $cat.time();
+
+	$thumbnail = $que.'t.webp';
+	$vid_tmp = $_FILES['vid1']['tmp_name'];
+	$thumbnail1_tmp = $_FILES['thumbnail1']['tmp_name'];
+	$destination_URL = '../img/'.$folder;
+
+	$usersView->imgProcessor($thumbnail, $thumbnail1_tmp, $destination_URL);
+	
+	$userData = $usersView->fetchUser();
+	$me = $userData[0]['profile_id'];
+
+	/////generate contentID
+	$contentID = $me.'_0_'.$cat.'_'.time();
+	$contentID_enc =  $usersView->enc_cons($contentID);
+
+
+	//if it is seeder, fill seeder tbl
+	$val3 = array('topic'=>$topic, 'cat'=>$cat, 'content_id'=>$contentID_enc);
+	$usersContr->insert('seeder', $val3);
+
+
+    $pw = '1234567890';
+    $dir = '../videos/'.$folder.'/';
+    $videoName = mt_rand(100, 999).$me.time().$cat.'.webm';
+    $vd_name = $usersView->videoEncryptor($vid_tmp, $pw, $dir, $videoName);
+
+
+	$media = 3;
+	$usersView->postSoloChat($cat, $videoName, $thumbnail, $media, $contentID_enc, $insight, $datetime);
+	echo json_encode(array("cat"=>$cat, "que"=>$que));	
+
+}
+if(isset($_POST['procedure'])){
+	var_dump('expression----------');
+	$insight = $_POST['insight'];
+	$sdID = $_POST['project'];
+//	$shrdKey = $_POST['sk'];
+
+	$sdData = $usersView->select('seeder', ' WHERE sd_id = ?', $sdID);
+	$projectID = $sdData[0]['content_id'];
+	$cat = $sdData[0]['cat'];
+	
+	$contentID_enc =  $projectID;
+
+	$datetime = $_POST['date'].' '.$_POST['time'];
+
+	    $aoi_array = $usersView->aoi($cat);
+		$folder = $aoi_array['folder'];
+
+	$que = time();
+
+	$img = $que.'.webp';
+
+	/*if( isset($_FILES['img']['tmp_name'])){
+		$img_tmp = $_FILES['img']['tmp_name'];
+		$destination_URL = '../img/'.$folder;
+		$usersView->imgProcessor($img, $img_tmp, $destination_URL);
+	}*/
+	$userData = $usersView->fetchUser();
+	$me = $userData[0]['profile_id'];
+
+
+    if( isset($_FILES['vid']['tmp_name'])){
+
+    	$vid_tmp = $_FILES['vid']['tmp_name'];
+	
+	    $pw = '1234567890';
+	    $dir = '../videos/'.$folder.'/';
+	    
+	    $videoName = mt_rand(100, 999).$me.time().$cat.'.webm';
+	    $vd_name = $usersView->videoEncryptor($vid_tmp, $pw, $dir, $videoName);
+    }
+
+	$media = 3;
+	$usersView->postSoloChat($cat, $videoName, 'thumbnail', $media, $contentID_enc, $insight, $datetime);
+	echo json_encode(array("cat"=>$cat, "que"=>$que));	
+
+}
+if(isset($_POST['publishSeed'])){
+
+	$caption = $_POST['insight'];
+	$sdID = $_POST['project'];
+	$sdData = $usersView->select('seeder', ' WHERE sd_id = ?', $sdID);
+	$projectID = $sdData[0]['content_id'];
+	$topic_id = $sdData[0]['cat'];
+	
+	$contentID_enc =  $projectID;
+
+	$datetime = $_POST['date'].' '.$_POST['time'];
+
+    $aoi_array = $usersView->aoi($topic_id);
+	$folder = $aoi_array['folder'];
+
+
+		$que = time();
+
+	$thumb2 = $que.'.webp';
+
+	if( isset($_FILES['thumb2']['tmp_name'])){
+		$thumb2_tmp = $_FILES['thumb2']['tmp_name'];
+		$destination_URL = '../img/'.$folder;
+		$usersView->imgProcessor($thumb2, $thumb2_tmp, $destination_URL);
+	}
+	$userData = $usersView->fetchUser();
+	$me = $userData[0]['profile_id'];
+
+
+    if( isset($_FILES['vid2']['tmp_name'])){
+
+    	$vid2_tmp = $_FILES['vid2']['tmp_name'];
+	
+	    $pw = '1234567890';
+	    $dir = '../videos/'.$folder.'/';
+	    
+	    $videoName = mt_rand(100, 999).$me.time().$topic_id.'.webm';
+	    $vd_name = $usersView->videoEncryptor($vid2_tmp, $pw, $dir, $videoName);
+    }
+
+	$media = 3;
+	$usersView->postSoloChat($topic_id, $videoName, 'thumbnail', $media, $contentID_enc, $caption, $datetime);
+
+
+
+    $searchkeys = $_POST['k1']." ".$_POST['k2']." ".$_POST['k3']." ".$_POST['k4']." ".$_POST['k5']." ".$_POST['k6'];
+    $_POST['authorization'] == 1 ? $authorization = $_POST['authority'] : $authorization = '0';
+    $chatpop = 's';
+  
+    $publish_mode = $_POST['pubmode'];
+    $publish_mode == 1 ? $price = 1.1 * intval($_POST['price']) : ($publish_mode == 2 ? $price = $_POST['launchfund'] : $price = 0);
+    $timespan = 1111111;
+    $shrdKey = $usersView->generateSoloSharedKey($topic_id);
+
+	$usersView->publish($contentID_enc, $caption, $searchkeys, $topic_id, $chatpop, $publish_mode, $authorization, $price, $timespan, $shrdKey);
+
+
+	echo json_encode(array("cat"=>$topic_id, "que"=>$que));	
+
+}
