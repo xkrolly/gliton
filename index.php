425056597c942b7724679dd523aa5ff2f2afae76
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-03-15 07:59:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $pgv->content='';
     $pgv->addCSS('https://fonts.googleapis.com/icon?family=Material+Icons');
   
 //if(isset($_SESSION['user_id']) || isset($_GET['autolog'])){
+var_dump($_SESSION['user_id']);
 if(isset($_SESSION['user_id']) && intval($_SESSION['user_id']) < 3){
   $pgv->addScript('scripts/bot.js');
   $pgv->addScript('scripts/recorder.js');
