3770b940ea1790765bd37efd251c6d85786b9d7d
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-03-30 15:27:01 +0100

    Update fixappo.php

--- views/fixappo.php
+++ views/fixappo.php
@@ -187,7 +187,7 @@ if(isset($_GET['rid'])){
         count($saturday) > 0 ? $sat .="</div></div>" : '';
 
     $output .="<div style='display:flex; flex-direction:column; align-items:center; padding:40px 20px 20px 20px; background:#fff; min-height:99vh;'><h5 style='font-size:14px; background:#2166f3; color:#fff; padding:5px; border-radius:4px;'>Fix appointment with ".$fullname."</h5><h6 style='margin:5px auto 20px auto; font-size:13px;'>Select from their free time below</h6><p style='color:red; font-style:italic; font-family:serif; font-size:10px; margin-top:-20px;'>Note: Each appointment last 30mins max.</p>";
-   $output .= "<div style='text-align:center;' class='chatInput'>".$sun.$mon.$tue.$wed.$thu.$fri.$sat."</div>";
+   $output .= "<div style='text-align:center; margin-bottom:50px;' class='chatInput'>".$sun.$mon.$tue.$wed.$thu.$fri.$sat."</div>";
    
     $output .="    <div style='position:fixed; bottom:2px; width:100%; background:#fff; padding:5px;'>
                         <form action='tutor' method='post' style='display:inline; width:100%;'>
