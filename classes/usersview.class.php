3fec7120b90bc25550f0e8f544b7462a7768ccd6
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-04-05 02:11:58 +0100

    Update usersview.class.php

--- classes/usersview.class.php
+++ classes/usersview.class.php
@@ -165,7 +165,7 @@ class UsersView extends Users {
               </div>
               <div style='display:flex; align-items:center; justify-content:flex-start; padding:8px; border-bottom:1px solid #ccc;'>
                 <span class='material-icons' style='color:#2166f3; margin-right:5px; font-size:25px;'>&#xe064;</span>
-                <a href='https://glit.ng/glitforbiz' target='_blank' style='font-weight:bold; text-decoration:none; font-size:14px;'>Glit for Business</a>
+                <a href='https://glit.ng/glit' target='_blank' style='font-weight:bold; text-decoration:none; font-size:14px;'>Glit for Business</a>
               </div>
               <div style='display:flex; align-items:center; justify-content:flex-start; padding:8px;'>
                 <span class='material-icons' style='color:red; margin-right:5px; font-size:25px;'>&#xe9ba;</span>
