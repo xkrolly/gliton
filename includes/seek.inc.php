40eb688d2a356f45988c66fce5ad2550f549d5f7
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-05-05 20:42:30 +0100

    Update seek.inc.php

--- includes/seek.inc.php
+++ includes/seek.inc.php
@@ -8,8 +8,39 @@ $usersView = new usersView();
 $userData = $usersView->fetchUser();
 $user = $userData[0]['profile_id'];
 $newList = $usersView->newList($user);
+
+$thirdpartyProdList = $usersView->thirdPPL($user);
+$prodListArr = explode(', ', $thirdpartyProdList);//['a', 'b', 'c'];//
+
 //var_dump($newList);
 $newListArr = explode(', ', $newList);
-$n = count($newListArr);
 
-echo json_encode(array("newList"=>$newList, "n"=>$n));
+//////////////////////////////////
+$finalArr = [];
+/*foreach (array_map(null, $newListArr, $prodListArr) as $pair) {
+	// code...
+	$finalArr = array_merge($finalArr, array_filter($pair));
+}
+*/
+$i = 0;
+$j = 0;
+
+while($i < count($newListArr) || $j < count($prodListArr)){
+	$step = rand(0, 3);
+	$step = min($step, count($newListArr) - $i);
+
+	for($k = 0; $k < $step && $i < count($newListArr); $k++){
+		$finalArr[] = $newListArr[$i];
+		$i++;
+	}
+	if($j < count($prodListArr)){
+		$finalArr[] = $prodListArr[$j];
+		$j++;
+	}
+}
+
+$n = count($finalArr);
+
+$finalList = implode(', ', $finalArr);
+//var_dump($finalArr);
+echo json_encode(array("newList"=>$finalList, "n"=>$n));
