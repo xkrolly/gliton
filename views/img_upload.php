b5dcc95f941ae81b98989f7eb9e47cd157e034a4
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-05-05 20:49:18 +0100

    Update img_upload.php

--- views/img_upload.php
+++ views/img_upload.php
@@ -1,23 +1,20 @@
 <?php
 
 include('../includes/autoloader.inc.php');
-    $usersView = new usersView();
-    $usersContr = new usersContr();
+  $usersView = new usersView();
+  $usersContr = new usersContr();
 
-  
-//capture img for storage
-  $img_input=$_FILES['img_data']['tmp_name'];
   $cat = $_POST['cat'];
-//  $imgName = $_POST['mediaProof'];
-  $imgName = mt_rand(1000, 9999).$uid.time().$cat;
- 
-  $cat_arr = $usersView->aoi($cat);
-  $folder = $cat_arr['folder'];
-  move_uploaded_file($img_input, '../img/'.$folder.'/'.$imgName.'.webp');
+  $imgusage = $_POST['imgusage'];
+  if($imgusage == 'productimg1' || $imgusage == 'productimg2'){
 
-$userData = $usersView->fetchUser();
-$me = $userData[0]['profile_id'];
-$vals = $imgName.', '.$me;
-$usersContr->update('profile', 'save_que = ?  WHERE profile_id = ?', $vals);
+    $prdImg = $_FILES['product_img']['tmp_name'];
+    $usersView->imguploader3p($cat, $prdImg, $imgusage);
   
- 
+  }
+  else{
+
+    $img_input=$_FILES['img_data']['tmp_name'];
+    $usersView->imguploader($cat, $img_input);
+  
+  }
