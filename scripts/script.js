45361582aff18cdf57126ff5a51528900d73b06f
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-05-19 07:01:35 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -333,7 +333,11 @@ function pageView(content){
                   document.getElementById('mediaA').play();
 
                                     ///////////////////3ppp
-	          if(tppimg == ''){document.getElementById('productIMG').style.display = 'none'; document.getElementById('productIMG2').style.display = 'none';}
+	          if(tppimg == ''){
+			  document.getElementById('productIMG').style.display = 'none'; document.getElementById('productIMG2').style.display = 'none';
+  			$('#like').setAttribute('onclick', 'likeDis("fpp");');
+
+		  }
 
                     if(tppimg != ''){
                       tppimg2 = tppimg+'_2';
@@ -341,6 +345,7 @@ function pageView(content){
                       document.getElementById('productIMG2').style.display = 'block';
                       document.getElementById('productIMG').src = 'img3pp/'+strDir+'/'+tppimg+'.webp';
                       document.getElementById('productIMG2').src = 'img3pp/'+strDir+'/'+tppimg2+'.webp';
+  			$('#like').setAttribute('onclick', 'likeDis("tpp");');
                        
                     }
 
