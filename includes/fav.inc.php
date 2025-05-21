8372c966e8782656ae462c3636f254fed9934577
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-05-21 22:56:40 +0100

    Update fav.inc.php

--- includes/fav.inc.php
+++ includes/fav.inc.php
@@ -9,17 +9,18 @@ include('autoloader.inc.php');
 if($regCompleted == 1){
 
     $_prdID = $_POST['prdID'];
-    $PI_typeCol = 'favs';
     $PI_totalCol = 'tF';
+    $prdType = $_POST['val'];
+    $prdType == 'tpp' ? $PI_typeCol = '3ppfavs' : $PI_typeCol = 'favs';
 
-    $dynamus = $usersView->productInteraction($_prdID, $PI_typeCol, $PI_totalCol);
-//var_dump($dynamus);
+    $dynamus = $usersView->productInteraction($_prdID, $PI_typeCol, $PI_totalCol, $prdType);
+
     $addFav = $dynamus[0];
     $newtotalFavs = $dynamus[1];
     $prdID = $dynamus[2];
     $pubid = $dynamus[3];
 
-    echo json_encode(array('status' => $addFav, 'ttf'=>$newtotalFavs, 'prdid'=>$prdID_, 'pubid'=>$pubid));
+    echo json_encode(array('status' => $addFav, 'ttf'=>$newtotalFavs, 'prdid'=>$prdID_, 'pubid'=>$pubid, 'prdType'=>$prdType));
 }
 else{
 		return $usersView->completeRegFirst();
