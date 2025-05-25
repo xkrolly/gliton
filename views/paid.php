b894e7fa1800d4787ed94a7dde3a8473a345a06a
xkrolly <99707749+xkrolly@users.noreply.github.com>
2025-05-25 05:47:21 +0100

    Update paid.php

--- views/paid.php
+++ views/paid.php
@@ -81,18 +81,18 @@ if(!empty($_GET['link'])){
 			   <div style='display:flex; height:50px; width:50px; margin-bottom:20px; margin-left:auto; margin-right:auto; border-radius:50%; border:2px solid #0f0; align-items:center; justify-content:center;'>
         			<span class='material-icons' style='color:#0f0; font-size:60px;'>&#xe86c;</span>
                            </div>
-			   <h2>Awesome!!! </h2>
-			   <div style='font-size:20px; font-family:serif; margin-top:20px;'>Your purchase of <span style='font-style:italic; text-decoration:underline; font-family:serif;'>".$heading."</span> 
+			   <h3>Awesome!!! </h3>
+			   <div style='font-size:16px; font-family:serif; margin-top:20px;'>Your purchase of <span style='font-style:italic; text-decoration:underline; font-family:serif;'>".$heading."</span> 
                 		<span style='font-style:italic; font-family:serif;'>solscript</span> was successful and your Glitcoin balance is <strong>$coinBal</strong><sup style='font-size:8px;'>GC</sup>. You can now freely access it through search and/or scrolls. Thanks. 
 					<div style='display:flex; justify-content:center; align-items:center; margin:30px auto 0 auto;'>
 						<a href='index.php?page=download&catid=$catID&scriptid=$_published&chat=$chatpop' style='font-style:italic; color:#fff; background:#2166f3; font-size:16px; padding:10px;'>Download</a> 
-						<div style='height:14px; width:2px; background:#000; margin-left:35px; margin-right:35px;'></div>
-						<a href='index.php?page=peepChats&s=$tutor&r=$student&pub=$pubid' style='font-style:italic; color:#2166f3; background:#fff; border:2px solid #2166f3; padding:10px; font-size:16px;'>View script</a>
+						<div style='height:14px; width:1px; background:#000; margin-left:15px; margin-right:15px;'></div>
+						<a href='index.php?page=peepChats&s=$tutor&r=$student&pub=$pubid' style='font-style:italic; color:#2166f3; background:#fff; border:2px solid #2166f3; padding:8px; font-size:16px;'>View script</a>
 					</div>
-					<div style='width:100%; font-style:italic; display:flex; flex-direction:column; justify-content:center; align-items:center; margin-top:50px; padding:10px; font-size:12px; background:#eee; border:1px solid #000; border-radius:20px;'>
-     						<div style='font-size:18px; font-weight:1000; font-family:roboto; margin-bottom:20px;'>Get a discount!</div>					
-     						<div style='font-size:14px; font-family:roboto;'>Get a discount, opportunity to impact people and access to unlimited earning from this solscript by creating a <span style='color:#2166f3; font-weight:bold;'>solution trial</span> with it!</div>
-	   					<div style='font-size:12px; font-family:roboto;'> <br>You dont want to miss out on this huge offer! All it takes is to just record a solscript about your experience when applying the solution through the link below.</div>
+					<div style='width:100%; font-style:italic; display:flex; flex-direction:column; justify-content:center; align-items:center; margin-bottom:10px; margin-top:50px; padding:10px; font-size:12px; background:#eee; border:1px solid #000; border-radius:20px;'>
+     						<div style='font-size:16px; font-weight:1000; font-family:roboto; margin-bottom:20px;'>Get a discount!</div>					
+     						<div style='font-size:13px; font-family:roboto;'>Get a discount, opportunity to impact people and access to unlimited earning from this solscript by creating a <span style='color:#2166f3; font-weight:bold;'>solution trial</span> with it!</div>
+	   					<div style='font-size:12px; font-family:roboto; font-style:italic;'> <br>You dont want to miss out on this huge offer! All it takes is to just record a solscript about your experience when applying the solution through the link below.</div>
 	   
 	  					<a href='index.php?page=trial&pubid=$pubid&catid=$catID' style='color:#000; padding:10px; margin-right:10px; text-decoration:underline; border-radius:10px;' class='hover'>Trial offer - 10% discount</a>
 	   				</div>
